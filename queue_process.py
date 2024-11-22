3db6ed1246cda86c86e09ab20ef5289a9a9c48d4
Drackout <mikatx2@hotmail.com>
2024-11-22 21:35:09 +0000

    Newfixes on the exercise

--- queue_process.py
+++ queue_process.py
@@ -7,15 +7,22 @@ stackPalavras = deque()
 
 palavraSplit = palavras.split(" ")
 
+hasO = -1
+
 for x in palavraSplit:
-    stackPalavras.append(x)
+    stackPalavras.appendleft(x)
 
 print(stackPalavras)
 
-for y in stackPalavras:
-    hasO = str(y).find("o")
+for o in range(len(stackPalavras)-1, -1, -1):
+    hasO = str(stackPalavras[o]).find("o")
     if hasO != -1:
-        print(y)
+        print(stackPalavras[o])
+
+# for y in stackPalavras:
+#     hasO = str(y).find("o")
+#     if hasO != -1:
+#         print(y)
             
 
 
